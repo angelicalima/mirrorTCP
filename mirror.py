540fa8c60dedf748913c4ce30aba71e672c655ed
angelicalima <lima.angelica@outlook.com>
2016-04-25 15:58:58 -0300

    correcao de problemas de conexao

--- mirror.py
+++ mirror.py
@@ -2,17 +2,18 @@ import socket
 import os
 from datetime import datetime
 import threading
+from binascii import *
 
 class mirror:
     nome_pasta_logs = 'log_mirror'
     nome_arquivo_log = 'log_' + str(datetime.now().replace(microsecond=0)).replace(' ', '_').replace(':', '').replace(
         '-', '') + '.txt'
-    rcv = ''
+    pct = ''
     try:
         os.mkdir(nome_pasta_logs)
     except:
         pass
-
+    th_lista = []
     arquivo_cfg = open('config.txt', 'r')
     arquivo_log = open(nome_pasta_logs+'//'+nome_arquivo_log,'w',0)
 
@@ -24,50 +25,78 @@ class mirror:
     print "Redirecionando para porta TCP: "+ str(porta_tcp_enviar)
 
     def start_recebe(self):
-        # Criando socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Servidor
         server_address = ('localhost', self.porta_tcp_receber)
         sock.bind(server_address)
         sock.listen(1)
+        i = 0
+        while True:
+            try:
+                nome_thread = threading.Thread(target = self.cria_lista_conexoes,args=(sock,server_address,  ))
+                self.th_lista.append(nome_thread)
+                nome_thread.start()
+            except:
+                pass
+            self.finaliza_conexoes(i)
+            i = i+1
+
+    def cria_lista_conexoes(self, sock, server_address ):
         while True:
             connection, client_address = sock.accept()
+            r_client_address = ('localhost', self.porta_tcp_enviar)
+            sock_cliente = socket.create_connection(r_client_address)
             try:
                 while True:
-                    self.pct = connection.recv(1024)
-                    if self.pct:
-                        print "Recebido: " + self.prefixo_log(server_address)+ " " + self.pct
-                        self.arquivo_log.write(self.prefixo_log(server_address) + self.pct)
-                    else:
-                        self.arquivo_log.write(self.prefixo_log(server_address) + 'conexao encerrada pelo cliente: ' + client_address)
-                        break
+                    try:
+                        self.pct = connection.recv(1024)
+                        if len(self.pct)>0:
+                            try:
+                                sock_cliente.send(self.pct)
+                                th_replica = threading.Thread(target= self.replica_comandos, args=(sock_cliente,connection,client_address,r_client_address, ))
+                                th_replica.start()
+                                print "Recebido: " + self.prefixo_log(client_address)+ " " + hexlify(self.pct)
+                                self.arquivo_log.write("Recebido: " + self.prefixo_log(client_address)+ " " + hexlify(self.pct) +"\n")
+                                print 'Enviado: ' + self.prefixo_log(r_client_address)+' '+hexlify(self.pct)
+                                self.arquivo_log.write('Enviado: ' + self.prefixo_log(r_client_address)+' '+ hexlify(self.pct)+"\n")
+                            except:
+                                sock.close()
+                                th_replica.join(1)
+                                break
+                                pass
+                    except:
+                        pass
+                        try:
+                            connection, client_address = sock_cliente.accept()
+                        except:
+                            print "conexao encerrada pelo cliente: " + str(connection)
+                            self.arquivo_log.write(self.prefixo_log(server_address) + 'conexao encerrada pelo cliente: ' + str(client_address)+"\n")
+                            break
+
             finally:
                 connection.close()
 
-    def start_envia(self):
+    def replica_comandos(self,sock_cliente,connection,client_address,r_client_address):
         while True:
-            # Criando socket
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # Cliente
-            client_address = ('localhost', self.porta_tcp_enviar)
-            sock = socket.create_connection(client_address)
-            while True:
-                try:
-                    if self.pct:
-                        sock.send(self.pct)
-                        print 'Enviado: ' + self.prefixo_log(client_address)+' '+self.pct
-                finally:
-                    sock.close()
+            try:
+                data = sock_cliente.recv(1024)
+                connection.send(data)
+                self.arquivo_log.write('Enviado de '+ str(r_client_address) + " para " + str(client_address) +': '+ hexlify(data)+"\n")
+                print 'Enviado de '+ str(r_client_address) + " para " + str(client_address) +': '+ hexlify(data)
+            except:
+                pass
 
-    @staticmethod
+    def finaliza_conexoes(self,i):
+        if self.th_lista[i].isAlive():
+            self.th_lista[i].join(1)
+
     def prefixo_log(self,server_address):
-        prefixo = str(server_address) + ' ' + str(datetime) + ': '
+        prefixo = str(server_address) + ' ' + str(datetime.now()) + ': '
         return prefixo
 
 
 if __name__ == "__main__":
     mirror = mirror()
     thread_recebe = threading.Thread(target=mirror.start_recebe)
-    thread_envia = threading.Thread(target=mirror.start_envia)
     thread_recebe.start()
-    thread_envia.start()
+
+
